8f17f8adc4b54b542f2e9f1d19e8f70fb184c1ae
PIANO09.26 <gogohiromi1007@gmail.com>
2026-09-26 16:05:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // 宿題手帳：オフラインでも開けるようにするための仕組み（記録はここには保存しません）
-const CACHE = "shukudai-techo-v8";
-const FILES = ["./", "./index.html", "./guide.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
+const CACHE = "shukudai-techo-v10";
+const FILES = ["./", "./index.html", "./guide.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 // 新しい版があればそれを使い、電波がないときは保存済みの版を表示
